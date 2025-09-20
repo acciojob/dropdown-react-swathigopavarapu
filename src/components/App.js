4c5f1aabe9ec8b3071da29fe7328331e42122c5c
Swathi Gopavarapu <swathigopavarapu1@gmail.com>
2025-09-20 16:10:03 +0530

    initial commit

--- src/components/App.js
+++ src/components/App.js
@@ -168,7 +168,7 @@ const states = [
       },
       {
         name: "Gaya",
-        description: "Gaya is a holy city beside the Falgu River, in the northeast Indian state of Bihar. It's known for 18th-century Vishnupad Mandir, a riverside temple with an octagonal shrine. Close by, ancient Mangla Gauri Temple is set on a hilltop.",
+        description: "Gaya is a holy city beside the Falgu River, in the northeast Indian state of Bihar. It's known for 18th-century Vishnupad Mandir, a riverside temple with an octagonal shrine. Close by, ancient Mangla Gauri Temple is set on a hilltop. ",
         landmarks: [
           {
             name: "Bakraur",
