aee831b1d13d7786ebca1eb2c3b6e9ce36fa25f1
Swathi Gopavarapu <swathigopavarapu1@gmail.com>
2025-09-20 15:24:45 +0530

    initial commit

--- src/components/App.js
+++ src/components/App.js
@@ -106,7 +106,7 @@ const states = [
     city: [
       {
         name: "Guwhati",
-        description: "Guwahati is a sprawling city beside the Brahmaputra River in the northeast Indian state of Assam. It's known for holy sites like the hilltop Kamakhya Temple.",
+        description: "Guwahati is a sprawling city beside the Brahmaputra River in the northeast Indian state of Assam. It's known for holy sites like the hilltop Kamakhya Temple,",
         landmarks: [
           {
             name: "Ganesh Guri",
